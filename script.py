74ad78869f28f074a0319647e00baf9b95e6b7f0
ericwujh <ericwujh@163.com>
2021-01-27 01:34:33 +0800

    Removed some code

--- script.py
+++ script.py
@@ -1,19 +1,8 @@
-import math
-import sys
-from os import rename
+
 
 import requests
 
-print(sys.version)
-print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
-
-
-print(greet('World'))
-print(greet('Corey'))
 r = requests.get('https://www.163.com')
 print(r.status_code)
